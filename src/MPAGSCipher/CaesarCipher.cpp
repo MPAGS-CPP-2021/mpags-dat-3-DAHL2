f11ea0fc9b9a839f443455451cf4bc912609715f
Dennis Lindebaum <dennis.lindebaum@bristol.ac.uk>
2021-10-29 10:47:38 +0000

    CaesarCipher class handling key processing.
    Additional validation function to test the key before asking for input

--- src/MPAGSCipher/CaesarCipher.cpp
+++ src/MPAGSCipher/CaesarCipher.cpp
@@ -1,5 +1,40 @@
 #include "CaesarCipher.hpp"
 
-CaesarCipher::CaesarCipher(const unsigned long cipherKey) : key_{cipherKey}
+#include <iostream>
+#include <string>
+
+CaesarCipher::CaesarCipher(const std::size_t cipherKey) : key_{cipherKey}
 {
+}
+
+CaesarCipher::CaesarCipher(const std::string cipherKey)
+{
+    key_ = std::stoul(cipherKey);
+}
+
+bool CaesarCipher::validateKey(const std::string testKey)
+{
+    // Retruns true if key is valid, false otherwise
+    for (const auto& elem : testKey) {
+        if (!std::isdigit(elem)) {
+            // We need to be able to check that the string contains a
+            // valid positive integer.
+            // Here we do that by looping through each character and checking that it
+            // is a digit. What is rather hard to check is whether the number is too
+            // large to be represented by an unsigned long, so we've omitted that for
+            // the time being.
+            // (Since the conversion function std::stoul will throw an exception if the
+            // string does not represent a valid unsigned long, we could check for and
+            // handled that instead but we only cover exceptions very briefly on the
+            // final day of this course - they are a very complex area of C++ that
+            // could take an entire course on their own!)
+            std::cerr
+                << "[error] cipher key must be an unsigned long integer for Caesar cipher,\n"
+                << "        the supplied key (" << testKey
+                << ") could not be successfully converted.\n"
+                << std::endl;
+            return false;
+        }
+    }
+    return true;
 }
--- src/MPAGSCipher/CaesarCipher.hpp
+++ src/MPAGSCipher/CaesarCipher.hpp
@@ -1,11 +1,14 @@
 #ifndef MPAGSCIPHER_CAESARCIPHER_HPP
 #define MPAGSCIPHER_CAESARCIPHER_HPP
 
-class CaesarCipher {
-    unsigned long key_;
+#include <string>
 
+class CaesarCipher {
   public:
-    CaesarCipher(const unsigned long cipherKey);
+    explicit CaesarCipher(const std::size_t cipherKey);
+    explicit CaesarCipher(const std::string cipherKey);
+    std::size_t key_{0};
+    static bool validateKey(const std::string testKey);
 };
 
 #endif    // MPAGSCIPHER_CAESARCIPHER_HPP
